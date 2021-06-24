be969baadbade774347f337f82c23caf0c2a2df6
Hakim Elmansouri <helmanso@e1r8p4.1337.ma>
2021-06-24 17:38:05 +0100

    delete old

--- status_manager.py
+++
@@ -1,8 +0,0 @@
-def update_status(commands_list):
-    for command in commands_list :
-        for sub in command.result:
-            if (isinstance(sub[1], str) is False):
-                if sub[1].poll() != None:
-                    sub[2] = "FINISHED"
-                else :
-                    sub[2] = "RUNNING"
